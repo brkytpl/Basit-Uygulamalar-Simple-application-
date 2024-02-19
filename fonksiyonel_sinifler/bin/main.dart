5a9666684e838ca2bd934b7d98ee7d044f43a514
Berkay Topal <brkytpl@gmail.com>
2024-02-19 21:30:39 +0300

    static sınıflar

---
+++ fonksiyonel_sinifler/bin/main.dart
@@ -0,0 +1,9 @@
+import 'matematik.dart';
+
+void main() {
+  print(Matematik.pi);
+  print(Matematik.topla(5, 6));
+  print(Matematik.bolme(4, 2));
+  print(Matematik.carpma(8, 12));
+  print(Matematik.cikarma(84, 52));
+}
---
+++ fonksiyonel_sinifler/bin/matematik.dart
@@ -0,0 +1,19 @@
+class Matematik {
+  static const pi = 3.14;
+
+  static topla(double sayi1, double sayi2) {
+    return sayi1 + sayi2;
+  }
+
+  static cikarma(double sayi1, double sayi2) {
+    return sayi1 - sayi2;
+  }
+
+  static carpma(double sayi1, double sayi2) {
+    return sayi1 * sayi2;
+  }
+
+  static bolme(double sayi1, double sayi2) {
+    return sayi1 / sayi2;
+  }
+}
